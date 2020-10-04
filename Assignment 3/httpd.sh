b1fa9088d7cd63aa846bd726bd9af81c02cb49d1
aantu014 <50641330+aantu014@users.noreply.github.com>
2020-10-04 02:09:53 -0400

    Update httpd.sh

--- Assignment 3/httpd.sh
+++ Assignment 3/httpd.sh
@@ -1,3 +1,10 @@
+firewall-cmd --permanent --zone=public --add-port=21/tcp
+firewall-cmd --permanent --zone=public --add-port=22/tcp
+firewall-cmd --permanent --zone=public --add-port=53/tcp
+firewall-cmd --permanent --zone=public --add-port=143/tcp
+firewall-cmd --permanent --zone=public --add-port=443/tcp
+firewall-cmd --reload
+
 mkdir /var/www/html
 
 cat aantu014.csv | cut -d "," -f1,2,3,4,5,6,7,8,9,10,11 | while IFS="," read username first last gender dob country color fruits os shell permission;
